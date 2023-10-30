f09f3a7f39741e92534c6e2a52481c4561eaed66
Mary Webby <118230483+marywebby@users.noreply.github.com>
2023-10-30 19:53:43 +0000

    all currency exchanges work

--- app.rb
+++ app.rb
@@ -3,6 +3,8 @@ require "sinatra/reloader"
 
 require "http"
 
+# render all the currencies 
+
 get("/") do
 
   api_url = "https://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
@@ -13,7 +15,40 @@ get("/") do
 
   parsed_data = JSON.parse(raw_data_string)
 
-  @symbols = parsed_data 
+  @symbols = parsed_data.fetch("currencies")
 
   erb(:homepage)
+end
+
+
+# this page will render the currency you choose to all the other currencys 
+get "/:from_currency" do
+  @from_currency = params[:from_currency]
+  api_url = "https://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
+
+  raw_data = HTTP.get(api_url) 
+  
+  raw_data_string = raw_data.to_s
+
+  parsed_data = JSON.parse(raw_data_string)
+
+  @symbols = parsed_data.fetch("currencies")
+
+  erb(:conversion_page)
+end
+
+
+get "/:from_currency/:to_currency" do
+  @from_currency = params[:from_currency]
+  @to_currency = params[:to_currency]
+  
+  api_url = "https://api.exchangerate.host/convert?access_key=#{ENV["EXCHANGE_RATE_KEY"]}&from=#{@from_currency}&to=#{@to_currency}&amount=1"
+  raw_data = HTTP.get(api_url)
+  parsed_data = JSON.parse(raw_data.to_s)
+  @conversion_rate = parsed_data.fetch("result")
+  
+  # Calculate the converted amount
+  @converted_amount = 1 * @conversion_rate
+  
+  erb(:results)
 end
